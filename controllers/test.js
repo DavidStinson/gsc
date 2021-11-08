ddbee655b4b0735b7db22b5c048cd539b5e44ff9
David Stinson <mail@davidstinson.dev>
2021-11-08 06:53:47 -0600

    test with real data

--- controllers/test.js
+++ controllers/test.js
@@ -6,16 +6,16 @@ function index(req, res) {
   
   const sheets = google.sheets({version: 'v4', auth: req.googleOAuthClient});
   sheets.spreadsheets.values.get({
-    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-    range: 'Class Data!A2:E',
+    spreadsheetId: '1Q2_IsFYekwpiafXPMVlxGJw0Q9SeEbxREzRZaGqkwkw',
+    range: 'A2:E',
   }, (err, response) => {
     if (err) return console.log('The API returned an error: ' + err);
     const rows = response.data.values;
     if (rows.length) {
-      console.log('Name, Major:');
+      console.log('Name, Project Name, Project Planning Materials, GitHub Link, Deployment Link');
       // Print columns A and E, which correspond to indices 0 and 4.
       rows.map((row) => {
-        console.log(`${row[0]}, ${row[4]}`);
+        console.log(`${row[0]}, ${row[1]}, ${row[2]}, ${row[3]}, ${row[4]}`);
       });
     } else {
       console.log('No data found.');
--- routes/test.js
+++ routes/test.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import * as testCtrl from "../controllers/test.js"
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
-router.get('/', testCtrl.index)
+router.get('/', isLoggedIn, testCtrl.index)
 
 export {
   router
