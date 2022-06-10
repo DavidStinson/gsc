0e7c94f7f2b0dc0517ed8016141d3c070452a979
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2022-06-09 22:23:49 -0500

    remove unused functions

--- helpers/g-sheets-lib.js
+++ helpers/g-sheets-lib.js
@@ -11,23 +11,6 @@ async function getRangeValuesFromSpreadsheet(sheets, spreadsheetId, range) {
   return spreadsheetRange.data.values
 }
 
-async function getRangesFromSpreadsheet(sheets, spreadsheetId, ranges) {
-  const spreadsheetRange = await sheets.spreadsheets.get({
-    spreadsheetId,
-    ranges
-  })
-  return spreadsheetRange.data.namedRanges
-}
-
-async function updateSpreadsheet(sheets, spreadsheetId, request) {
-  await sheets.spreadsheets.values.update({
-    spreadsheetId,
-    range: "C3:C7",
-    resource: request,
-    valueInputOption: "RAW",
-  })
-}
-
 async function batchUpdateSpreadsheet(sheets, spreadsheetId, request) {
   await sheets.spreadsheets.values.batchUpdate({
     spreadsheetId,
@@ -40,8 +23,6 @@ async function batchUpdateSpreadsheet(sheets, spreadsheetId, request) {
 
 export {
   getSpreadsheet,
-  getRangesFromSpreadsheet,
   getRangeValuesFromSpreadsheet,
-  updateSpreadsheet,
   batchUpdateSpreadsheet,
 }
